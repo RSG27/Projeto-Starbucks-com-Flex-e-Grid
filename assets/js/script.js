f0e291293743e442a5fc050819bf3a159373c536
RSG26 <ruangaspar1383@gmail.com>
2022-06-02 18:44:19 -0300

    correcao do js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,3 @@ function dissapearingButton() {
 }
 
 window.addEventListener('scroll', dissapearingButton);
-
-window.onload = function() {
-  document.querySelector(".menu-opener").addEventListener("click", function() {
-      if (document.querySelector(".menu nav").style.display == 'flex') {
-          document.querySelector(".menu nav").style.display = 'none';
-      } else {
-          document.querySelector(".menu nav").style.display = 'flex';
-      }
-  });
-};
